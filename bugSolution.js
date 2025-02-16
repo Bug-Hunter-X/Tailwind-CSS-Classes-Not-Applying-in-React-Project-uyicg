300be2516cad675ef05ebcacf7f47a85dc922ac8
BugHunter-X <askzenai@gmail.com>
2025-02-16 06:46:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+```javascript
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      Hover over me!
+    </div>
+  );
+};
+
+export default MyComponent;
+```
